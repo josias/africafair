c4ebf94fe14723796e5c545e7367a1a517d5ad35
Josias Dossougoin <josiadoss@gmail.com>
2021-11-13 11:25:39 +0100

    fixing static files issue

--- af/settings/production.py
+++ af/settings/production.py
@@ -30,6 +30,11 @@ MIDDLEWARE = [
     
 ]
 
+# Static files (CSS, JavaScript, Images)
 STATIC_ROOT =  [BASE_DIR /'staticfiles',]
+STATIC_URL = '/static/'
+
+# Extra places for collecstatic
+STATICFILES_DIRS = [BASE_DIR /'static']
 
 DATABASES = {'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}

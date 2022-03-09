440103cf85ceb29a777d67a8edfc5513d6b93b37
Josias Dossougoin <josiadoss@gmail.com>
2022-03-09 13:13:20 +0100

    wsgi update

--- af/wsgi.py
+++ af/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'af.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'af.settings.production')
 
 application = get_wsgi_application()
